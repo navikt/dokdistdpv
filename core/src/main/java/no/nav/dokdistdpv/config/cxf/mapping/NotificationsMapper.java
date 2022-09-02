79155e8819a62c5e274cabc49336f318b346429d
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2022-09-02 15:05:12 +0200

    MMA-6359: Oppdaterte varseltekster utkast
    avventer videre til noen varslede endringer i spec kommer

--- core/src/main/java/no/nav/dokdistdpv/config/cxf/mapping/NotificationsMapper.java
+++ core/src/main/java/no/nav/dokdistdpv/config/cxf/mapping/NotificationsMapper.java
@@ -7,6 +7,7 @@ import no.altinn.correspondenceagencyexternalaec.ReceiverEndPointBEList;
 import no.altinn.correspondenceagencyexternalaec.TextToken;
 import no.altinn.correspondenceagencyexternalaec.TextTokenSubstitutionBEList;
 import no.nav.dokdistdpv.consumer.rdist001.domain.DistribusjonsTypeKode;
+import no.nav.dokdistdpv.consumer.rdist001.domain.HentForsendelseResponse;
 
 import static no.altinn.correspondenceagencyexternalaec.TransportType.EMAIL_PREFERRED;
 import static no.nav.dokdistdpv.config.cxf.mapping.AltinnForsendelseMapper.FROM_ADDRESS;
@@ -18,26 +19,26 @@ public class NotificationsMapper {
 	public final static String NOTIFICATION_FOR_ANNET = "VarselDPVUtenRevarsel";
 	public final static String NOTIFICATION_FOR_VEDTAK_VIKTIG_ELLER_IKKE_SATT = "VarselDPVMedRevarsel";
 
-	public static NotificationBEList mapNotifications(DistribusjonsTypeKode distribusjonsTypeKode) {
+	public static NotificationBEList mapNotifications(HentForsendelseResponse forsendelse) {
 		NotificationBEList notificationList = new NotificationBEList();
 
 		Notification notification = new Notification();
 		notification.setFromAddress(FROM_ADDRESS);
 		notification.setLanguageCode(LANGUAGE_CODE_BOKMAAL);
-		notification.setNotificationType(mapNotificationType(distribusjonsTypeKode));
-		notification.setTextTokens(mapTextTokens(notification.getNotificationType()));
+		notification.setNotificationType(mapNotificationType(forsendelse.distribusjonstype()));
+		notification.setTextTokens(mapTextTokens(generateNotificationContentFor(forsendelse)));
 		notification.setReceiverEndPoints(mapReceiverEndpoints());
 		notificationList.getNotification().add(notification);
 
 		return notificationList;
 	}
 
-	private static TextTokenSubstitutionBEList mapTextTokens(String notificationType) {
+	private static TextTokenSubstitutionBEList mapTextTokens(String notificationTextContent) {
 		TextTokenSubstitutionBEList textTokenSubstitutionBEList = new TextTokenSubstitutionBEList();
 
 		TextToken textToken = new TextToken();
 		textToken.setTokenNum(1);
-		textToken.setTokenValue("Dummy test token for " + notificationType);
+		textToken.setTokenValue(notificationTextContent);
 		textTokenSubstitutionBEList.getTextToken().add(textToken);
 
 		return textTokenSubstitutionBEList;
@@ -59,5 +60,18 @@ public class NotificationsMapper {
 		} else {
 			return NOTIFICATION_FOR_VEDTAK_VIKTIG_ELLER_IKKE_SATT;
 		}
+	}
+
+	private static String generateNotificationContentFor(HentForsendelseResponse forsendelse) {
+		var nullSafeDistribusjonsTypeKode = forsendelse.distribusjonstype() == null ? ANNET : forsendelse.distribusjonstype();
+		return switch (nullSafeDistribusjonsTypeKode) {
+			case VEDTAK -> "Du har mottatt vedtak «";
+			case VIKTIG -> "Du har mottatt viktig brev «";
+			case ANNET -> "Du har mottatt melding «";
+		}
+				+ forsendelse.forsendelseTittel() +
+				"» fra NAV for " + forsendelse.mottaker().mottakerId() +  // $reporteeNumber$ ?
+				", " + forsendelse.mottaker().mottakerNavn() + // $reporteeName$ ?
+				" på www.altinn.no. Gå til min meldingsboks i Altinn for å lese det.";
 	}
 }
