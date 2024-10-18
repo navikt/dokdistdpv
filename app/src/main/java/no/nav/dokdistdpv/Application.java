bfdb9a20079eb9dc8bd7f801b5453adbb0ca0a25
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-10-18 16:05:59 +0200

    MMA-7642 Endret TLS oppsett i MQ
    * Endringen fjerner client authentication for TLS
    * Ryddet config

--- app/src/main/java/no/nav/dokdistdpv/Application.java
+++ app/src/main/java/no/nav/dokdistdpv/Application.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-import static java.lang.System.getenv;
-import static java.lang.System.setProperty;
-
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
 	public static void main(String[] args) {
-		setProperty("javax.net.ssl.keyStorePassword", getenv("DOKDISTDPVCERT_KEYSTORE_PASSWORD"));
 		SpringApplication.run(Application.class, args);
 	}
 }
