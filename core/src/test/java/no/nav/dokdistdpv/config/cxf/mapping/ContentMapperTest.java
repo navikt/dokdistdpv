397231134c7db263e7b5d2f51b2930540c4aad22
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2025-05-06 15:06:38 +0200

    MMA-8080 Oppdatert filename-mapping til Altinn
    * Vasker mulig ikke-tillatt `\t` tegn
    * Vasker trailing whitespace i tittel slik at filnavn ikke blir "tittel  .pdf"
    * Mapper filnavn slik at det ikke kan bli "tittel.pdf.pdf"
    * Mer error-logging fra Altinn siden (inkluderer extendedError og localizedError)

--- core/src/test/java/no/nav/dokdistdpv/config/cxf/mapping/ContentMapperTest.java
+++ core/src/test/java/no/nav/dokdistdpv/config/cxf/mapping/ContentMapperTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static no.nav.dokdistdpv.config.cxf.mapping.ContentMapper.mapFileName;
+import static no.nav.dokdistdpv.config.cxf.mapping.ContentMapper.mapFilename;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ContentMapperTest {
 
 	@ParameterizedTest
 	@MethodSource("provideFilenameTitles")
-	void shouldMapFilenameAndRemoveIllegalFilenameChars(String inputDokumenttittel, String expectedDokumenttittel) {
-		String actualDokumenttittel = mapFileName("1000", inputDokumenttittel);
+	void shouldMapAndCleanFilenames(String inputDokumenttittel, String expectedFilename) {
+		String actualFilename = mapFilename("1000", inputDokumenttittel);
 
-		assertThat(actualDokumenttittel).isEqualTo(expectedDokumenttittel);
+		assertThat(actualFilename).isEqualTo(expectedFilename);
 	}
 
 	private static Stream<Arguments> provideFilenameTitles() {
@@ -26,7 +26,10 @@ class ContentMapperTest {
 				Arguments.of("Innvilget <15 pst. erstatning", "1000Innvilget 15 pst. erstatning.pdf"),
 				Arguments.of("XX                 Overs.brev", "1000XX                 Overs.brev.pdf"),
 				Arguments.of("Søknad om hjelpemiddel / ortopedisk middel", "1000Søknad om hjelpemiddel  ortopedisk middel.pdf"),
-				Arguments.of("Ettersending av\\\\/:*?\"<>| dokumentasjon", "1000Ettersending av dokumentasjon.pdf")
+				Arguments.of("Ettersending av\\\\/:*?\"<>| dokumentasjon", "1000Ettersending av dokumentasjon.pdf"),
+				Arguments.of("Helseskjema d.t\t.05.05.25\t", "1000Helseskjema d.t.05.05.25.pdf"),
+				Arguments.of("Dokument (Uten relevant innhold)   ", "1000Dokument (Uten relevant innhold).pdf"),
+				Arguments.of("Uønsket_hendelse_avvik_HMS_Utsatt_for_mye_marsipankake_ABCDEF5_000.pdf", "1000Uønsket_hendelse_avvik_HMS_Utsatt_for_mye_marsipankake_ABCDEF5_000.pdf")
 		);
 	}
 }
