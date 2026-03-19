a005927d012e3bc4c646bc27a7dcd131cfdd83c3
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-03-19 11:16:24 +0100

    MMA-8619 Begrenser filnavn i zip til 255 bytes
    * Siden vi ikke vet systemet til brukeren som skal åpne zip så begrenser vi filnavn for entries til 255 bytes (ext4)
    * Nettlesere og moderne zip-verktøy vil som regel korrigere for dette
    * La til test-jar i `core`

--- core/src/main/java/no/nav/dokdistdpv/consumer/altinn/map/AttachmentNameMapper.java
+++ core/src/main/java/no/nav/dokdistdpv/consumer/altinn/map/AttachmentNameMapper.java
@@ -1,5 +1,9 @@
 package no.nav.dokdistdpv.consumer.altinn.map;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Pattern;
 
 import static org.apache.commons.lang3.StringUtils.left;
@@ -7,15 +11,19 @@ import static org.apache.commons.lang3.StringUtils.removeEnd;
 import static org.apache.commons.lang3.StringUtils.removeEndIgnoreCase;
 import static org.apache.commons.lang3.StringUtils.trim;
 
-/// Helper for å mappe fileName og displayName
-/// Vasker navn for ugyldige tegn
+/// Mapper for filnavn og visningsnavn. Vasker ugyldige tegn og begrenser lengden
+///
+/// * [Windows](https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#file-and-directory-names)
 public class AttachmentNameMapper {
 	private static final String BLANK_ERSTATNING = "";
 	private static final String PUNKTUM = ".";
+	/// Altinn API: 255 chars
+	/// Windows: 255 chars
+	/// Apple File System: 255 chars
+	/// ext4: 255 bytes
 	private static final int FILE_NAME_MAX_CHARS = 255;
 	private static final int DISPLAY_NAME_MAX_CHARS = 255;
-	// https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#file-and-directory-names
-	private static final Pattern FILE_NAME_UGYLDIGE_TEGN = Pattern.compile("[<>:\"/\\\\|?*\\u0000-\\u001f]");
+	private static final Pattern FILE_NAME_UGYLDIGE_TEGN_WINDOWS = Pattern.compile("[<>:\"/\\\\|?*\\u0000-\\u001f]");
 	private static final Pattern DISPLAY_NAME_UGYLDIGE_TEGN = Pattern.compile("\\u0000");
 	private static final String PDF_FILENDELSE = ".pdf";
 	private static final int FILE_NAME_DIFF = FILE_NAME_MAX_CHARS - PDF_FILENDELSE.length();
@@ -28,6 +36,14 @@ public class AttachmentNameMapper {
 		return left(fjernUgyldigeFileNameTegn(removeEnd(trim(fjernPdfFilendelse(trim(tittel))), PUNKTUM)), FILE_NAME_DIFF) + PDF_FILENDELSE;
 	}
 
+	public static String mapZipEntryFileName(long arkivDokumentInfoId, String tittel) {
+		return mapZipEntryFileName(arkivDokumentInfoId + "_" + tittel);
+	}
+
+	public static String mapZipEntryFileName(String tittel) {
+		return leftBytes(fjernUgyldigeFileNameTegn(removeEnd(trim(fjernPdfFilendelse(trim(tittel))), PUNKTUM))) + PDF_FILENDELSE;
+	}
+
 	public static String mapDisplayName(String tittel) {
 		return left(fjernUgyldigeDisplayNameTegn(trim(tittel)), DISPLAY_NAME_MAX_CHARS);
 	}
@@ -37,10 +53,27 @@ public class AttachmentNameMapper {
 	}
 
 	private static String fjernUgyldigeFileNameTegn(String tekst) {
-		return FILE_NAME_UGYLDIGE_TEGN.matcher(tekst).replaceAll(BLANK_ERSTATNING);
+		return FILE_NAME_UGYLDIGE_TEGN_WINDOWS.matcher(tekst).replaceAll(BLANK_ERSTATNING);
 	}
 
 	private static String fjernUgyldigeDisplayNameTegn(String tekst) {
 		return DISPLAY_NAME_UGYLDIGE_TEGN.matcher(tekst).replaceAll(BLANK_ERSTATNING);
+	}
+
+	/// Util for å beholde de første `tekst` bytes opp til `FILE_NAME_DIFF`
+	/// Hindrer at unicode chars blir delt opp
+	private static String leftBytes(String tekst) {
+		if (tekst == null) return null;
+
+		if (tekst.length() <= FILE_NAME_DIFF) {
+			return tekst;
+		}
+
+		CharsetEncoder encoder = StandardCharsets.UTF_8.newEncoder();
+		CharBuffer charBuffer = CharBuffer.wrap(tekst);
+		ByteBuffer byteBuffer = ByteBuffer.allocate(FILE_NAME_DIFF);
+		encoder.encode(charBuffer, byteBuffer, true);
+		byteBuffer.flip();
+		return new String(byteBuffer.array(), 0, byteBuffer.limit(), StandardCharsets.UTF_8);
 	}
 }
