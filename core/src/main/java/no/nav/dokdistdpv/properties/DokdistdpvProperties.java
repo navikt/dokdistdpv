fd63f70ed97ea325175760e60584311fd488d7e7
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-04-29 14:38:28 +0200

    MMA-8675 Avvikle sdist007
    * Fjernet sdist007 kode og konfigurasjon
    * Endret fnr i test til noe åpenbart syntetisk

--- core/src/main/java/no/nav/dokdistdpv/properties/DokdistdpvProperties.java
+++ core/src/main/java/no/nav/dokdistdpv/properties/DokdistdpvProperties.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
@@ -19,8 +18,6 @@ public class DokdistdpvProperties {
 	@Valid
 	private final Endpoints endpoints = new Endpoints();
 	@Valid
-	private final Sdist007 sdist007 = new Sdist007();
-	@Valid
 	private final Topics topic = new Topics();
 
 	@Data
@@ -29,17 +26,6 @@ public class DokdistdpvProperties {
 		private boolean altinn3;
 		@Positive
 		private int altinn3VedleggZipGrense;
-	}
-
-	@Data
-	public static class Sdist007 {
-		private boolean autostartup;
-		@NotEmpty
-		private String cronScheduler;
-		@PositiveOrZero
-		private int fraAntallEkspedertDagerTilbake;
-		@PositiveOrZero
-		private int tilAntallEkspedertDagerTilbake;
 	}
 
 	@Data
