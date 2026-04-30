b6300d44f85f021e830df0ad26f6db12047fcd69
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-04-30 09:50:25 +0200

    MMA-8675 Fjernet print kø konfigurasjon, håndteres av distribuerTilNyKanal

--- core/src/main/java/no/nav/dokdistdpv/consumer/rdist001/AdministrerForsendelseConsumer.java
+++ core/src/main/java/no/nav/dokdistdpv/consumer/rdist001/AdministrerForsendelseConsumer.java
@@ -2,11 +2,9 @@ package no.nav.dokdistdpv.consumer.rdist001;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokdistdpv.consumer.rdist001.domain.DistribuerTilNyKanalRequest;
-import no.nav.dokdistdpv.consumer.rdist001.domain.DistribusjonsTypeKode;
 import no.nav.dokdistdpv.consumer.rdist001.domain.FinnForsendelseRequest;
 import no.nav.dokdistdpv.consumer.rdist001.domain.FinnForsendelseResponse;
 import no.nav.dokdistdpv.consumer.rdist001.domain.HentForsendelseResponse;
-import no.nav.dokdistdpv.consumer.rdist001.domain.HentForsendelserResponse;
 import no.nav.dokdistdpv.consumer.rdist001.domain.OppdaterForsendelseRequest;
 import no.nav.dokdistdpv.exception.AdministrerForsendelseFunctionalException;
 import no.nav.dokdistdpv.exception.AdministrerForsendelseTechnicalException;
@@ -20,17 +18,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClientRequest;
-
-import java.time.Duration;
-import java.util.List;
 
 import static java.lang.String.format;
-import static no.nav.dokdistdpv.consumer.rdist001.domain.DistribusjonKanal.DPVT;
-import static no.nav.dokdistdpv.consumer.rdist001.domain.DistribusjonsTypeKode.VEDTAK;
-import static no.nav.dokdistdpv.consumer.rdist001.domain.DistribusjonsTypeKode.VIKTIG;
-import static no.nav.dokdistdpv.consumer.rdist001.domain.DokumentStatus.EKSPEDERT;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -39,7 +28,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Component
 public class AdministrerForsendelseConsumer {
 
-	private static final List<DistribusjonsTypeKode> DISTRIBUSJONS_TYPER = List.of(VEDTAK, VIKTIG);
 	private final WebClient webClient;
 
 	public AdministrerForsendelseConsumer(WebClient webClient,
@@ -90,45 +78,6 @@ public class AdministrerForsendelseConsumer {
 				.block();
 
 		oppdaterForsendelseLog(oppdaterForsendelse);
-	}
-
-	@Retryable(retryFor = AdministrerForsendelseTechnicalException.class)
-	public HentForsendelserResponse hentForsendelser(List<String> journalpostliste) {
-		return webClient.get()
-				.uri(uriBuilder -> uriBuilder.path("/hentForsendelser")
-						.queryParam("distribusjonstyper", DISTRIBUSJONS_TYPER)
-						.queryParam("dokumentstatus", EKSPEDERT)
-						.queryParam("distribusjonkanal", DPVT)
-						.queryParam("inkluderAvstemte", false)
-						.queryParam("journalpostliste", String.join(",", journalpostliste))
-						.build())
-				.httpRequest(clientHttpRequest -> {
-					HttpClientRequest nativeRequest = clientHttpRequest.getNativeRequest();
-					nativeRequest.responseTimeout(Duration.ofSeconds(180));
-				})
-				.retrieve()
-				.bodyToMono(HentForsendelserResponse.class)
-				.onErrorResume(Throwable.class, err -> {
-					log.warn("hentForsendelser feilet med feilmelding={}", err.getMessage(), err);
-					return Mono.empty();
-				})
-				.block();
-	}
-
-	@Retryable(retryFor = AdministrerForsendelseTechnicalException.class)
-	public void oppdaterForsendelserAvstemtDatoOgReferanse(OppdaterForsendelserAvstemtInfo oppdaterForsendelserAvstemtInfo) {
-		log.info("oppdaterForsendelserAvstemDatoOgReferanse har mottatt kall om å oppdatere {} forsendelser med avstemtReferanse={}",
-				oppdaterForsendelserAvstemtInfo.getForsendelser().size(), oppdaterForsendelserAvstemtInfo.getAvstemtReferanse());
-		webClient.put()
-				.uri("/avstemforsendelser")
-				.bodyValue(oppdaterForsendelserAvstemtInfo)
-				.retrieve()
-				.toBodilessEntity()
-				.doOnSuccess(response ->
-						log.info("avstemforsendelser har oppdatert {} forsendelser med avstemtReferanse og avstemtDato", oppdaterForsendelserAvstemtInfo.getForsendelser().size()))
-				.doOnError(Throwable.class, err ->
-						log.warn("oppdaterForsendelserAvstemtDatoOgReferanse feilet med feilmelding={}", err.getMessage()))
-				.block();
 	}
 
 	private void oppdaterForsendelseLog(OppdaterForsendelseRequest oppdaterForsendelse) {
--- core/src/main/java/no/nav/dokdistdpv/utils/DokdistdpvConstant.java
+++ core/src/main/java/no/nav/dokdistdpv/utils/DokdistdpvConstant.java
@@ -3,7 +3,6 @@ package no.nav.dokdistdpv.utils;
 public class DokdistdpvConstant {
 
 	public static final String PROPERTY_FORSENDELSE_ID = "forsendelseId";
-	public static final String PROPERTY_JOURNALPOST_ID = "journalpostId";
 	public static final String CALL_ID = "callId";
 	public static final String NAV_CONSUMER_ID = "Nav-Consumer-Id";
 	public static final String MDC_REQUEST_ID = "requestId";
