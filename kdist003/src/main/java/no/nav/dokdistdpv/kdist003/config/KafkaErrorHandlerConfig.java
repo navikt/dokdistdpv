c666f9b41d890ffc359252c4ada64637726537b9
Joakim Borgersen <joakim.borgersen@nav.no>
2026-05-29 12:55:11 +0200

    Fix backoff og hopp over meldinger som ikke kan deserializes

--- kdist003/src/main/java/no/nav/dokdistdpv/kdist003/config/KafkaErrorHandlerConfig.java
+++ kdist003/src/main/java/no/nav/dokdistdpv/kdist003/config/KafkaErrorHandlerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
+import org.springframework.kafka.support.serializer.DeserializationException;
 
 @Configuration
 public class KafkaErrorHandlerConfig {
@@ -14,8 +15,13 @@ public class KafkaErrorHandlerConfig {
 	@ConditionalOnMissingBean(CommonErrorHandler.class)
 	public CommonErrorHandler kafkaErrorHandler() {
 
-		// maxRetries=10, initialInterval=2000ms, multiplier=1.5s gir totalt ~2min 34s fra første forsøk til siste forsøk
+		// maxRetries=10, initialInterval=2000ms, multiplier=1.5 gir totalt ~2min 34s fra første forsøk til siste forsøk
 		var backOff = new ExponentialBackOffWithMaxRetries(10);
-		return new DefaultErrorHandler(backOff);
+		backOff.setInitialInterval(2000L);
+		backOff.setMultiplier(1.5);
+
+		var errorHandler = new DefaultErrorHandler(backOff);
+		errorHandler.addNotRetryableExceptions(DeserializationException.class);
+		return errorHandler;
 	}
 }
